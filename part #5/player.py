0dbd44a32a38db8a879e67b10f9a1114b594deeb
StanislavPetrovV <34447380+StanislavPetrovV@users.noreply.github.com>
2020-07-04 13:36:28 +0800

    part #5

---
+++ part #5/player.py
@@ -0,0 +1,49 @@
+from settings import *
+import pygame
+import math
+
+class Player:
+    def __init__(self):
+        self.x, self.y = player_pos
+        self.angle = player_angle
+        self.sensitivity = 0.004
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
+    def movement(self):
+        self.keys_control()
+        self.mouse_control()
+        self.angle %= DOUBLE_PI
+
+    def keys_control(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            exit()
+
+        if keys[pygame.K_w]:
+            self.x += player_speed * cos_a
+            self.y += player_speed * sin_a
+        if keys[pygame.K_s]:
+            self.x += -player_speed * cos_a
+            self.y += -player_speed * sin_a
+        if keys[pygame.K_a]:
+            self.x += player_speed * sin_a
+            self.y += -player_speed * cos_a
+        if keys[pygame.K_d]:
+            self.x += -player_speed * sin_a
+            self.y += player_speed * cos_a
+        if keys[pygame.K_LEFT]:
+            self.angle -= 0.02
+        if keys[pygame.K_RIGHT]:
+            self.angle += 0.02
+
+    def mouse_control(self):
+        if pygame.mouse.get_focused():
+            difference = pygame.mouse.get_pos()[0] - HALF_WIDTH
+            pygame.mouse.set_pos((HALF_WIDTH, HALF_HEIGHT))
+            self.angle += difference * self.sensitivity
+
